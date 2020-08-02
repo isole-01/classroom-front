69f173db9ecf8d5a643568d66073fe0354163759
isole-01 <captain.atr007@gmail.com>
2020-08-02 19:28:54 +0430

    Updates

---
+++ precache-manifest.bc9f503088eaf90f835109cc5e168076.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf6a88635605c9c7f179cea7e631786",
+    "url": "/classroom-front/index.html"
+  },
+  {
+    "revision": "6e3b4f6da6dd21014a01",
+    "url": "/classroom-front/static/css/main.a49495ea.chunk.css"
+  },
+  {
+    "revision": "0bef3199899020931cc3",
+    "url": "/classroom-front/static/js/2.1910f18a.chunk.js"
+  },
+  {
+    "revision": "6e3b4f6da6dd21014a01",
+    "url": "/classroom-front/static/js/main.e7cd26e9.chunk.js"
+  },
+  {
+    "revision": "235861f89dede286fd7c",
+    "url": "/classroom-front/static/js/runtime~main.4c677459.js"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/classroom-front/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "d30c9e30978c9278180dcce2bb66c054",
+    "url": "/classroom-front/static/media/sidebar-2.d30c9e30.jpg"
+  }
+]);
